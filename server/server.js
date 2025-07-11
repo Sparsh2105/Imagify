9c03cef0ceddeca42686b6e8de82f486cdad877c
Sparsh2105 <sparshchaudhary.jee@gmail.com>
2025-07-11 07:23:18 +0530

    a

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ import 'dotenv/config'
 import connectDB from '../server/config/mongodb.js'
 import userRouter from './routes/userRoutes.js'
 import imageRouter from './routes/imageRoutes.js'
-const PORT= process.env.PORT
+const PORT= process.env.PORT|| 4000
 const app=express()
 app.use(cors())
 
